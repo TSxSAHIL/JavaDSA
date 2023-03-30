b383024d958c8d2524eae4301fe85ffd0cb63662
TSxSAHIL <tssahil2001@gmail.com>
2023-03-30 18:13:56 +0530

    30-03

--- Hash.java
+++ Hash.java
@@ -1,12 +1,21 @@
-import java.*;
+import java.util.*;
 public class Hash {
     public static void main(String[] args) {
-        HashMap<String, Integer> maps = new Hashmap<>();
+        HashMap<String, Integer> maps = new HashMap<>();
         maps.put("Indian", 120);
         maps.put("Russian", 12000);
         maps.put("Nepali", 20);
 
         System.out.println(maps);
+        if(maps.containsKey("China")){
+            System.out.println("Present in the Map");
+        }else{
+            System.out.println("Absent in the Map");
+        }
+        for(Map.Entry<String, Integer> e: maps.entrySet()){
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
 
     }
 }
