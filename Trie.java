5b4d91484175095c436e157d00b619fa0a899778
TSxSAHIL <tssahil2001@gmail.com>
2023-04-06 17:47:27 +0530

    06-04-2023

---
+++ Trie.java
@@ -0,0 +1,44 @@
+public class Trie {
+
+    static class Node{
+        Node[] children;
+        boolean eow;
+
+        public Node(){
+            children = new Node[26];
+            for(int i = 0 ; i<26 ; i++){
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+
+    static Node root = new Node();
+
+    public static void insert(String word) { //O(n)
+        int level = 0;
+        int len = word.length();
+        int idx = 0;
+ 
+ 
+        Node curr = root;
+        for(; level<len; level++) {
+            idx = word.charAt(level)-'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+ 
+
+    public static void main(String[] args) {
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+        for (String word : words) {
+            insert(word);
+            System.out.println("inserted " + word);
+        }
+ 
+    }
+}
