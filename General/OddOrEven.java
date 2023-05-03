bc514899fbc3fb8a7873afa0582b12356511f862
TSxSAHIL <tssahil2001@gmail.com>
2023-05-03 10:15:38 +0530

    .

---
+++ General/OddOrEven.java
@@ -0,0 +1,15 @@
+
+
+public class Main
+ {
+      public static void main(String[] args) {
+           int number = 29;
+
+
+     //checking whether the number is even or odd
+     if (number % 2 == 0)
+              System.out.println(number + " is Even");
+     else
+              System.out.println(number + " is odd");
+      }
+ }
