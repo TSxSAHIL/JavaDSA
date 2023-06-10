c38e1b1112328db84e91ecae1470ace2e3c28d7e
TSxSAHIL <tssahil2001@gmail.com>
2023-06-10 22:56:06 +0530

    10june

---
+++ Arrays/Mainy.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Mainy {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        char[] s1 = s.toCharArray();
+        Arrays.sort(s1);
+        // System.out.println(s1);
+        String s2 = new String(s1);
+        // System.out.println(s2);
+
+        char[] t1 = t.toCharArray();
+        Arrays.sort(t1);
+        // System.out.println(t1);
+        String t2 = new String(t1);
+        // System.out.println(t2);
+
+        if(s2.equals(t2)){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
