7df84e527cf88ea71bf32802c59ea1ef80f2ea69
TSxSAHIL <tssahil2001@gmail.com>
2023-06-29 09:55:58 +0530

    29-06

---
+++ Queue/Queue.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Queue {
+    public static void main(String[] args) {
+        int[] array = {20,5,6,15,4,2};
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0 ; i<array.length; i++){
+            arr.add(array[i]);
+        }
+        int total_time = 0;
+        for(int i = 0 ; i<arr.size() ; i++){
+            if(arr.get(i)>6){
+                total_time += 7;
+                arr.add(arr.get(i)-6);
+            }
+            else if(arr.get(i)<=6){
+                total_time += 2;
+            }
+        }
+
+        System.out.println(total_time);
+    }    
+}
