78591fa0a6b0bbcf059a5f59de91b73c9f407dbe
TSxSAHIL <tssahil2001@gmail.com>
2023-06-29 10:28:59 +0530

    2906

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Queue {
     public static void main(String[] args) {
-        int[] array = {20,5,6,15,4,2};
+        int[] array = {20,5,6,25,4,2};
         ArrayList<Integer> arr = new ArrayList<>();
         for(int i = 0 ; i<array.length; i++){
             arr.add(array[i]);
@@ -9,14 +9,21 @@ public class Queue {
         int total_time = 0;
         for(int i = 0 ; i<arr.size() ; i++){
             if(arr.get(i)>=5){
-                total_time += 7;
-                arr.add(arr.get(i)-6);
+                if(arr.get(i)>6){
+                    arr.add(arr.get(i)-6);
+                }
             }
-            else if(arr.get(i)<5){
+            
+        }
+        for(int i = 0 ; i<arr.size() ; i++){
+            if(arr.get(i)>=5){
+                total_time += 7;
+            }
+            else{
                 total_time += 2;
             }
         }
         System.out.println(arr);
-        System.out.println("The Total Time is: " +total_time);
+        System.out.println(total_time);
     }    
 }
