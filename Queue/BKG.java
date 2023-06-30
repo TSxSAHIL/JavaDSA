940197b428ec68de66ceddbcf07e218cb612f69c
TSxSAHIL <tssahil2001@gmail.com>
2023-06-30 22:08:57 +0530

    3006

---
+++ Queue/BKG.java
@@ -0,0 +1,28 @@
+class BKG {
+    public static int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            if(!map.containKey(num)){
+                map.put(num, map.get(num) + 1);
+            }
+            
+            else{
+                map.put(num,1);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer key : map.keySet()) {
+            value = map.get(key);
+            if(value>2){
+                list.add(map.get(key));
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        int []nums = {1,1,1,2,2,3};
+        int k = 2;
+        System.out.println(topKFrequent(nums, k));
+
+    }
+}
