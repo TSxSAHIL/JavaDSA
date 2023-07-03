0eecd4776813f10e05a723c3b081177450ab208c
TSxSAHIL <tssahil2001@gmail.com>
2023-07-03 09:44:34 +0530

    .

--- Dynamic Programming/SubSetSumDp.java
+++ Dynamic Programming/SubSetSumDp.java
@@ -8,6 +8,7 @@ class SubSetSumDp {
         for (int i = 0; i <= n; i++)
             dp[i][0] = true;
 
+            
         // DP calculation
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= targetSum; j++) {
