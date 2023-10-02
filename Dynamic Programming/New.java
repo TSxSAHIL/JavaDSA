a73839e12ab8817351dfcf0020dd6e65201a45e2
TSxSAHIL <tssahil2001@gmail.com>
2023-10-02 22:06:55 +0530

    .

--- Dynamic Programming/New.java
+++ Dynamic Programming/New.java
@@ -1,28 +1,23 @@
-import java.util.Arrays;
+// Queue using two stacks
 
-public class New {
-    public static int maxEnvelopes(int[][] envelopes) {
-        int n = envelopes.length;
-        if (n == 0) {
-            return 0;
-        }
-        Arrays.sort(envelopes, (a, b) -> a[0] - b[0]);
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int max = 1;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            max = Math.max(max, dp[i]);
-        }
-        return max;
+public class New{
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 2, 1, 2, 1};
+        int n = arr.length;
+        System.out.println(FrogJumps(arr, n));
     }
 
-    public static void main(String[] args) {
-        int[][] envelopes = {{5,4},{6,4},{6,7},{2,3}};
-        System.out.println(maxEnvelopes(envelopes)); // Output: 3
+    public static int FrogJumps(int[] arr, int n){
+        int[] dp = new int[n];
+        dp[0] = 0;
+        for(int i = 1; i < n; i++){
+            dp[i] = Integer.MAX_VALUE;
+            for(int j = 0; j < i; j++){
+                if(arr[j] + j >= i){
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n-1];
     }
 }
