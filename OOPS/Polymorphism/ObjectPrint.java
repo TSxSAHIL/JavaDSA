3def04c33f788f14990ddfeee406120ef5b24d25
TSxSAHIL <tssahil2001@gmail.com>
2023-10-26 21:08:09 +0530

    .

---
+++ OOPS/Polymorphism/ObjectPrint.java
@@ -0,0 +1,14 @@
+public class ObjectPrint {
+    int num;
+    public ObjectPrint(int num){
+        this.num = num;
+    }
+    @Override
+    public String toString(){
+        return "ObjectPrint{"+"num"+num+'}';
+    }
+    public static void main(String[] args) {
+        ObjectPrint obj = new ObjectPrint(69);
+        System.out.println(obj);
+    }
+}
