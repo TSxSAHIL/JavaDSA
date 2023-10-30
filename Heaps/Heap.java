820b889d203071031ca9fa69ad4a9b7c668a8b8c
TSxSAHIL <tssahil2001@gmail.com>
2023-10-31 02:25:41 +0530

    .

--- Heaps/Heap.java
+++ Heaps/Heap.java
@@ -19,8 +19,85 @@ class Heap <T extends Comparable<T>>{
     }
     private void Swap(int first , int sec){
         T temp = list.get(first);
-        list.get(first) = list.get(sec);
         list.set(first , list.get(sec));
         list.set(sec, temp);
+    }
+
+    private int parent (int index){
+        return (index-1)/2;
+    }
+    private int left(int index){
+        return (2*index)+1;
+    }
+    private int right(int index){
+        return (2*index)+2;
+    }
+
+    public void insert(T value){
+        list.add(value);
+        upheap(list.size()-1);
+    }
+
+    private void upheap(int index){
+        if(index == 0){
+            return;
+        }
+        int p = parent(index);
+        if(list.get(index).compareTo(list.get(p))<0){
+            Swap(index,p);
+            upheap(p);
+        }
+    }
+
+    public T remove() throws Exception{
+        if(list.isEmpty()){
+            throw new Exception("Removing from an empty heap");
+        }
+        T temp = list.get(0);
+        T last = list.remove(list.size()-1);
+        if(!list.isEmpty()){
+            list.set(0, last);
+            downheap(0);
+        }
+        return temp;
+    }
+    
+    private void downheap (int index){
+        int min = index;
+        int left = left(index);
+        int right = right(index);
+
+        if(left<list.size()&&list.get(min).compareTo(list.get(left))>0){
+            min = left;
+        }
+        if(right<list.size()&&list.get(min).compareTo(list.get(right))>0){
+            min = right;
+        }
+        if(min != index){
+            Swap(min, index);
+            downheap(min);
+        } 
+    }
+
+    public ArrayList<T> heapSort() throws Exception{
+        ArrayList<T> data = new ArrayList<>();
+        while (!list.isEmpty()) {
+            data.add(this.remove());
+        }
+        return data;
+    }
+
+    public static void main(String[] args) throws Exception{
+        Heap<Integer> heap = new Heap<>();
+        heap.insert(34);
+        heap.insert(45);
+        heap.insert(56);
+        heap.insert(22);
+        heap.insert(76);
+
+        ArrayList<Integer> list = heap.heapSort();
+        System.out.println(list);
+
+        
     }
 }
