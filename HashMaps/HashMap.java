a2a2bd44f1d525b2202b5d6479170b2936266949
TSxSAHIL <tssahil2001@gmail.com>
2023-10-31 14:16:16 +0530

    .

--- HashMaps/HashMap.java
+++ HashMaps/HashMap.java
@@ -1,6 +1,12 @@
 /*
     HashMaps Data Structure
     We use hashmaps becuase of constant time complexity.
+    1. We need all elements ad positive numbers -> Hashcode function
+    2. Hashcode can be very large so reduce it, it is called hashing
+    
+    ** Two ways to avoid collions
+    1. Chaining 
+    2. Open Addressing
 
  */
 class HashMap {
