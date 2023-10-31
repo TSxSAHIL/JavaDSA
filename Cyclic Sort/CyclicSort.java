cc9a5d1cd522e7c4c711523121ede39f64cf7f2b
TSxSAHIL <tssahil2001@gmail.com>
2023-10-31 22:28:39 +0530

    .

--- Cyclic Sort/CyclicSort.java
+++ Cyclic Sort/CyclicSort.java
@@ -6,6 +6,31 @@
     ->    Worst Time Complexity -> O(2N-1) or O(N)
     
  */
+
 class CyclicSort {
-    
+    public static void main(String[] args) {
+        int arr [] = {3,5,2,1,4};
+        cyclicSort(arr);
+        for(int a : arr){
+            System.out.print(a + " ");
+        }
+    }
+
+    public static void cyclicSort(int arr[]){
+        int i = 0 ;
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr , i , correct);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    public static void swap(int arr[] , int i , int correct){
+        int temp = arr[i];
+        arr[i] = arr[correct];
+        arr[correct] = temp;
+    }
 }
