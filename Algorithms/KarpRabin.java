1cf0acda2c1d0b5dc8242f90ef91079d3c63c0cf
TSxSAHIL <tssahil2001@gmail.com>
2023-11-01 18:45:30 +0530

    .

---
+++ Algorithms/KarpRabin.java
@@ -0,0 +1,14 @@
+/*
+    Karp Rabin Algorithm
+
+    string s = "sahil"
+    string t = "vijaywashingsahilrahul"
+
+    does string s exists in string t
+
+ */
+
+import java.util.*;
+class KarpRabin{
+
+}
