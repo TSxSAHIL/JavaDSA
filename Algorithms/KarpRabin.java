bb7fd7ca2affffdef0daa1463c5d5e7c5b23b9c1
TSxSAHIL <tssahil2001@gmail.com>
2023-11-01 18:52:07 +0530

    .

--- Algorithms/KarpRabin.java
+++ Algorithms/KarpRabin.java
@@ -8,6 +8,10 @@
     can we do this in linear time -> O(n+m)
     hashing is used in google search
 
+    h(a) = h(b)[i:i+len(a)]
+    1. h(a) = h(b)[i:i+len(a)] then maybe we have found a match check every character time complexity O(a)
+    
+
  */
 
 import java.util.*;
