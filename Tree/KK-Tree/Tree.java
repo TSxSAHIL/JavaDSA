28eb92f5f60f701b47c918408d25d98487d2cf18
TSxSAHIL <tssahil2001@gmail.com>
2023-11-14 16:39:38 +0530

    .

--- Tree/KK-Tree/Tree.java
+++ Tree/KK-Tree/Tree.java
@@ -9,7 +9,11 @@
     Types of Binary Tree
     1. Complete Binary Tree -> All levels are full but last level is empty from left tp roght
     2. Full Binary Tree / Strict Binary Tree -> Each node has either 0 or 2 children (No one Child)
-    3. 
+    3. Perfect Binary Tree -> All the internal nodes have two children and all the leaf nodes are on same level and all the level are full
+    4. Height Balanceed Binary tree -> Average Height O(log n)
+    5. Skewed Binary Tree -> Every node has only one child (Average height - > O(n) just like linked list)
+    6. Ordered Binary Tree -> Every node has some property. Example Binary Search Tree
+    
  */
 
 public class Tree {
